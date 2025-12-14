afeb3bf774cab0d086d56a2e4bde3027f923527b
Om <ompanem@gmail.com>
2025-12-13 22:48:38 -0600

    code for day 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String uri = "https://export.arxiv.org/api/query?search_query=all:electron&start=0&max_results=10";
+        HttpActions httpActions = new HttpActions(uri);
+
+    }
+
+
+}
